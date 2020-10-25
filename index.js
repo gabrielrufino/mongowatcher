97232228b0922aa75aba5cfb26deeef48c7effae
Gabriel Rufino <contato@gabrielrufino.com>
2020-10-25 16:36:24 -0300

    Initial index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const { program } = require('commander')
+
+const { version } = require('./package.json')
+
+program.version(version)
+
+async function main() {
+
+}
+
+main()
