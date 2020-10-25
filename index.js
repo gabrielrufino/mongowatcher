9a93de82761f739995a7a39eaecdfe6151ac5801
Gabriel Rufino <contato@gabrielrufino.com>
2020-10-25 16:48:24 -0300

    Receiving URI from cli

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 
 'use strict'
 
+const { MongoClient } = require('mongodb')
 const { program } = require('commander')
 
 const { version } = require('./package.json')
 
-program.version(version)
+program
+  .version(version)
+  .option('--uri <uri>', 'MongoDB instance URI')
+  .parse(process.argv)
 
 async function main() {
-
+  console.log(program.uri)
 }
 
 main()
